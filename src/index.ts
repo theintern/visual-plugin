ef132daaa4e61a6931cb7de9c96de05663842d0b
Paul Shannon <pshannon@sitepen.com>
2016-11-10 17:48:55 -0700

    cleaning up CJS and AMD exports

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,22 @@ import { default as config } from './config';
 import { default as visualTest } from './test';
 import * as file from './util/file';
 import getRGBA, { ColorDescriptor } from './util/getRGBA';
-import Test = require('intern/lib/Test');
+import * as resizeWindow from './helpers/resizeWindow';
 
 const util = {
 	file,
 	getRGBA
 };
 
+const helpers = {
+	resizeWindow
+};
+
 export {
 	assertVisuals,
+	ColorDescriptor,
 	config,
+	helpers,
 	util,
 	visualTest,
 };
